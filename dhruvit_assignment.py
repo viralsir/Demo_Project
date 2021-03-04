e450205925b758d8cf059482c9a5e6e99cbc721c
Dhruvit-Khajanchi <dhruvikh299@gmail.com>
2021-03-04 18:10:59 +0530

    Assignment complete

--- dhruvit_assignment.py
+++ dhruvit_assignment.py
@@ -1,1 +1,11 @@
 ''' display sum of given 5 no.  '''
+
+no1= int(input("Enter 1st number:  "))
+no2= int(input("Enter 2nd number:  "))
+no3= int(input("Enter 3rd number:  "))
+no4= int(input("Enter 4th number:  "))
+no5= int(input("Enter 5th number:  "))
+
+x= no5+no4+no2+no3+no1
+
+print("Addition of 5 Numbers is: ", x)
