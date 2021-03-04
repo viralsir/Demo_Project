594c1c444fdda6253bd2514dc60d293fa42f084d
sakshiahuja2026@gmail.com <sakshiahuja20262gmail.com>
2021-03-04 18:17:44 +0530

    Factorial Program

--- saskhi_assignment.py
+++ saskhi_assignment.py
@@ -1,1 +1,13 @@
 ''' display the factorial of given no'''
+a = int(input("Enter a number:"))
+fact = 1
+if a < 0:
+    print("not possible")
+elif a == 0:
+    print("fact is 1")
+else:
+    for i in range (1, a+1):
+        fact = fact * i
+print("The factorial of", a, "is", fact)
+
+
